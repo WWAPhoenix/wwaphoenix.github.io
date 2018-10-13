dd87809c152c461cd6c944f1eefabf23bd4523ac
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-10-13 23:02:13 +0900

    fix(jump-to-image): errorとprogressに対応

--- jump-to-image.js
+++ jump-to-image.js
@@ -1,4 +1,14 @@
 function postMessage_noWorker(response) {
-  location.href = "./" + response.data.wwaData.mapCGName;
+  if(response.data.wwaData) {
+    location.href = "./" + response.data.wwaData.mapCGName;
+  } else if (response.data.error) {
+    alert("エラーです:" + (error instanceof Error ? error.message : error));
+    console.error(error);
+  } else if (response.data.progress) {
+    var stage = responde.data.stage;
+    var current = response.data.current;
+    var total = response.data.total;
+    console.log("[WWALoader] stage" + stage + ": " + current + "/" + total);
+  }
 }
 
