9fd3f15c1d25466bc1d326ff1f8dcbef1d1484c0
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-10-13 23:09:05 +0900

    fix(jump-to-image):typo

--- jump-to-image.js
+++ jump-to-image.js
@@ -3,10 +3,10 @@ function postMessage_noWorker(response) {
     location.href = "./" + response.data.wwaData.mapCGName;
   } else if (response.data.error) {
     var error = response.data.error;
-    alert("エラーです:" + (error instanceof Error ? error.message : error));
+    alert("エラーです:" + (error.message ? error.message : error));
     console.error(error);
   } else if (response.data.progress) {
-    var stage = responde.data.stage;
+    var stage = response.data.stage;
     var current = response.data.current;
     var total = response.data.total;
     console.log("[WWALoader] stage" + stage + ": " + current + "/" + total);
